f2ed37366011fabec521eb1d9254f95342bfb80b
TheBitson124 <as.sosnowicz@gmail.com>
2024-04-12 14:19:58 +0200

    done

---
+++ WebApplication1/WebApplication1/Controllers/AnimalController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route ("api/{controller}")]
+public class AnimalController:ControllerBase
+{
+        [HttpGet]
+        public IActionResult GetAnimals()
+        {
+                return Ok();
+        }
+}
